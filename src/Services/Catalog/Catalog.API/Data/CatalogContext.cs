f86f8058db580aad4e15533deb6364e91f7632fb
Venkatachalam Govindhan <vengatachalamg4@gmail.com>
2023-04-30 18:28:08 +0530

    Latest code merge

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,19 @@
+﻿using Catalog.API.Enities;
+using MongoDB.Driver;
+using System;
+using Microsoft.Extensions.Configuration;
+namespace Catalog.API.Data
+{
+    public class CatalogContext:ICatalogContext
+    {
+      public CatalogContext(IConfiguration configuration) {
+        var client=new MongoClient(configuration.GetValue<string>("DataBaseSettings:ConnectionString"));
+            var database = client.GetDatabase(configuration.GetValue<string>("DatabaseSettings:DatabaseName"));
+             Products = database.GetCollection<Product>(configuration.GetValue<string>("DataBaseSettings:CollectionName"));
+            CatalogContextSeed.SeedData(Products);
+        }
+
+        //public IMongoCollection<Product> Products { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public IMongoCollection<Product> Products { get; }
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Data/ICatalogContext.cs
@@ -0,0 +1,12 @@
+﻿using Catalog.API.Enities;
+using MongoDB.Driver;
+
+
+namespace Catalog.API.Data
+{
+    public interface ICatalogContext
+    {
+        IMongoCollection<Product> Products { get; }
+
+    }
+}
